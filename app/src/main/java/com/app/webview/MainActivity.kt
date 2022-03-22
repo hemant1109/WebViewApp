6406d642c0b8a7186f12d82a1a20e5aa8eea20f9
Parvin Pathan <pgpathan22@gmail.com>
2022-03-22 18:27:20 +0530

    First commit.

---
+++ app/src/main/java/com/app/webview/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.app.webview
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.webkit.CookieManager
+import android.webkit.WebSettings.LOAD_NO_CACHE
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.app.webview.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var isReload: Boolean = true
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.cirProgressBar.isVisible = true
+
+        binding.webview.settings.javaScriptEnabled = true
+        // no cache
+        binding.webview.settings.cacheMode = LOAD_NO_CACHE
+        //end
+        // no cookie
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(false)
+        //end
+        binding.webview.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String, favicon: Bitmap?) {
+                Log.e("webViewClientStarted", "webViewClient $url")
+                binding.cirProgressBar.isVisible = true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                Log.e("webViewClientFinishe", "webViewClient $url")
+                binding.cirProgressBar.isVisible = false
+            }
+
+            override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+                super.doUpdateVisitedHistory(view, url, isReload)
+                Log.e("webViewClienHistory", "webViewClient $url")
+                Log.e("webViewClienHistory", "webViewClient $isReload")
+                this@MainActivity.isReload = isReload
+            }
+
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let {
+                    if (isReload) {
+                        return false
+                    }
+                    val browserIntent =
+                        Intent(Intent.ACTION_VIEW, Uri.parse(it))
+                    startActivity(browserIntent)
+                    Log.e("webViewClient", "webViewClient $it")
+                }
+                return true
+            }
+        }
+        binding.webview.loadUrl(BuildConfig.BASE_URL)
+
+    }
+}
